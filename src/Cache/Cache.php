b6a6258c1aa9361cd5d1707a143744fc1213d67b
webuser <ryan@virtuecenter.com>
2013-09-22 09:21:47 -0400

    addd getSetGet method

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -15,4 +15,16 @@ class Cache extends \Memcache {
 	public function get ($key, $flags=null) {
 		return parent::get($key, $flags);
 	}
+
+	public static function getSetGet ($key, $callback, $host='localhost', $port=11211) {
+		$cache = self::factory($host, $port);
+		$data = $cache->get($key, MEMCACHE_COMPRESSED);
+		if ($data === false) {
+			$data = $callback();
+			if ($data !== false) {
+				$cache->set($key, $data, MEMCACHE_COMPRESSED, 0);
+			}
+		}
+		return $data;
+	}
 }
