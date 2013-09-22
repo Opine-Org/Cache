f18ab21b71b66b9d2424cc40bfd148a43a1b1af4
webuser <ryan@virtuecenter.com>
2013-09-22 09:38:54 -0400

    add batch mode for getSetGet

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -16,15 +16,28 @@ class Cache extends \Memcache {
 		return parent::get($key, $flags);
 	}
 
-	public static function getSetGet ($key, $callback, $host='localhost', $port=11211) {
+	public static function getSetGet ($key, $callback, $ttl=0, $host='localhost', $port=11211) {
 		$cache = self::factory($host, $port);
 		$data = $cache->get($key, MEMCACHE_COMPRESSED);
 		if ($data === false) {
 			$data = $callback();
 			if ($data !== false) {
-				$cache->set($key, $data, MEMCACHE_COMPRESSED, 0);
+				$cache->set($key, $data, MEMCACHE_COMPRESSED, $ttl);
 			}
 		}
 		return $data;
 	}
+
+	public static function getSetGetBatch (Array &$keyCallbacks, $ttl=0, $host='localhost', $port=11211) {
+		$cache = self::factory($host, $port);
+		$data = $cache->get(array_keys($keyCallbacks), MEMCACHE_COMPRESSED);
+		foreach ($keyCallbacks as $key => &$item) {
+			if (!isset($data[$key]) || $data[$key] === false) {
+				$data[$key] = $item['callback']();
+				if ($data[$key] !== false) {
+					$cache->set($key, $data[$key], MEMCACHE_COMPRESSED, $ttl);
+				}
+			}
+		}
+	}	
 }
