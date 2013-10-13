e9787cdd8d06e2c5d822cf4e956b4086aa62a433
webuser <ryan@virtuecenter.com>
2013-10-12 22:43:44 -0400

    use dependency injection

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -2,16 +2,11 @@
 namespace Cache;
 
 class Cache {
-	private static $memcache = [];
+	private $memcache;
 	private $host;
 	private $port;
 
-	public function connnection ($host, $port) {
-		$this->host = $host;
-		$this->port = $port;
-	}
-
-	private static function check () {
+	private function check () {
 		if (!class_exists('\Memcache')) {
 			return false;
 		}
@@ -19,78 +14,77 @@ class Cache {
 	}
 
 	public function delete ($key, $timeout=0, $host='localhost') {
-		if (!self::check()) {
+		if (!$this->check()) {
 			return false;
 		}
-		$cache = self::factory($host, $port);
-		return $cache->delete($key, $timeout);
+		$this->memcache->pconnect($this->host, $this->port);
+		return $this->memcache->delete($key, $timeout);
 	}
 
-	public static function factory ($host='localhost', $port=11211) {
-		if (!self::check()) {
-			return new Cache();
+	public function __construct ($host='localhost', $port=11211) {
+		if (!$this->check()) {
+			return;
 		}
-		if (isset(self::$memcache[$host]) === false) {
-			self::$memcache[$host] = new \Memcache();
-			self::$memcache[$host]->connect($host, $port);
-		}
-		return self::$memcache[$host];
+		$this->host = $host;
+		$this->port = $port;
+		$this->memcache = new \Memcache();
 	}
 
 	public function set ($key, $value, $flag=null, $expire=0) {
-		if (!self::check()) {
+		if (!$this->check()) {
 			return false;
 		}
-		return $cache->set($key, $value, $flag, $expire);
+		$this->memcache->pconnect($this->host, $this->port);
+		return $this->memcache->set($key, $value, $flag, $expire);
 	}
 
 	public function get ($key, $options, $host='localhost', $port=11211) {
-		if (!self::check()) {
+		if (!$this->check()) {
 			return false;
 		}
-		$cache = self::factory($host, $port);
-		return $cache->get($key, MEMCACHE_COMPRESSED);
+		$this->memcache->pconnect($this->host, $this->port);
+		return $this->memcache->get($key, MEMCACHE_COMPRESSED);
 	}
 
-	public static function getSetGet ($key, $callback, $ttl=0, $host='localhost', $port=11211) {
-		if (!self::check()) {
+	public function getSetGet ($key, $callback, $ttl=0, $host='localhost', $port=11211) {
+		if (!$this->check()) {
 			return $callback();
 			return false;
 		}
-		$cache = self::factory($host, $port);
-		$data = $cache->get($key, MEMCACHE_COMPRESSED);
+		$this->memcache->pconnect($this->host, $this->port);
+		$data = $this->memcache->get($key, MEMCACHE_COMPRESSED);
 		if ($data === false) {
 			$data = $callback();
 			if ($data !== false) {
-				$cache->set($key, $data, MEMCACHE_COMPRESSED, $ttl);
+				$this->memcache->set($key, $data, MEMCACHE_COMPRESSED, $ttl);
 			}
 		}
 		return $data;
 	}
 
-	public static function getSetGetBatch (Array &$keyCallbacks, $ttl=0, $host='localhost', $port=11211) {
-		if (!self::check()) {
+	public function getSetGetBatch (Array &$keyCallbacks, $ttl=0, $host='localhost', $port=11211) {
+		if (!$this->check()) {
 			return false;
 		}
-		$cache = self::factory($host, $port);
-		$data = $cache->get(array_keys($keyCallbacks), MEMCACHE_COMPRESSED);
+		$this->memcache->pconnect($this->host, $this->port);
+		$data = $this->memcache->get(array_keys($keyCallbacks), MEMCACHE_COMPRESSED);
 		foreach ($keyCallbacks as $key => &$item) {
 			if (!isset($data[$key]) || $data[$key] === false) {
 				$data[$key] = $item['callback']();
 			}
 			if ($data[$key] !== false) {
-				$cache->set($key, $data[$key], MEMCACHE_COMPRESSED, $ttl);
+				$this->memcache->set($key, $data[$key], MEMCACHE_COMPRESSED, $ttl);
 			}
 		}
 	}
 
-	public static function getBatch (Array &$items, $host='localhost', $port=11211) {
-		if (!self::check()) {
+	public function getBatch (Array &$items, $host='localhost', $port=11211) {
+		if (!$this->check()) {
 			return false;
 		}
 		$count = sizeof($items);
-		$cache = self::factory($host, $port);
-		$data = $cache->get(array_keys($items), MEMCACHE_COMPRESSED);
+		$this->memcache->pconnect($this->host, $this->port);
+		$data = $this->memcache->get(array_keys($items), MEMCACHE_COMPRESSED);
 		$hits = 0;
 		foreach ($items as $key => &$item) {
 			if (isset($data[$key]) && $data[$key] !== false) {
@@ -104,13 +98,13 @@ class Cache {
 		return false;
 	}	
 
-	public static function deleteBatch (Array $items, $host='localhost', $port=11211) {
-		if (!self::check()) {
+	public function deleteBatch (Array $items, $host='localhost', $port=11211) {
+		if (!$this->check()) {
 			return false;
 		}
-		$cache = self::factory($host, $port);
+		$this->memcache->pconnect($this->host, $this->port);
 		foreach ($items as $item) {
-			$cache->delete($item);
+			$this->memcache->delete($item);
 		}
 	}
 }
